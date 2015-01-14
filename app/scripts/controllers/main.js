46713ad1c79db5e2706d1dd49ac3c5974d8feec0
Singh, Satnam <v-singhs-kfo@ae.com>
2015-01-14 12:48:23 -0800

    updated with more layout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,13 @@ angular.module('outpathsApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.className = 'glyphicon glyphicon-minus';
+    $scope.toggleClass = function(){
+      if ($scope.className === 'glyphicon glyphicon-plus'){
+        $scope.className = 'glyphicon glyphicon-minus';
+      }
+      else if ($scope.className === 'glyphicon glyphicon-minus'){
+        $scope.className = 'glyphicon glyphicon-plus';
+      }
+    };
   });
